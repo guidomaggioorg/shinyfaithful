e4093e95f4f6d66f8c9cad5aeca1dd96ecb303b0
GuidoMaggio <guido.maggio@mirai-solutions.com>
2024-05-16 17:19:38 +0200

    Added CD step

---
+++ deploy/deploy-shinyapps.R
@@ -0,0 +1,21 @@
+# deploy/deploy-shinyapps.R
+# usethis::use_build_ignore("deploy")
+if (!interactive()) {
+  rsconnect::setAccountInfo(
+    Sys.getenv("SHINYAPPS_ACCOUNT"),
+    Sys.getenv("SHINYAPPS_TOKEN"),
+    Sys.getenv("SHINYAPPS_SECRET")
+  )
+}
+# Add here any additional files/directories the app needs
+app_files = c(
+  "app.R",
+  "DESCRIPTION",
+  "NAMESPACE",
+  "R/",
+  "inst/"
+)
+rsconnect::deployApp(
+  account = "gmaggio", # not necessary
+  appName = "FaithfulPlots", appFiles = app_files, forceUpdate = TRUE
+)
